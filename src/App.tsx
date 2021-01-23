1cc939799d1f44f34e83910c547374e2e8b35989
Hemadri Dasari <hemadridasari@Hemadris-MacBook-Pro.local>
2021-01-23 12:27:57 +0400

    Add Material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
+import { MuiThemeProvider, withStyles } from '@material-ui/core/styles'
+import React, { Component, Suspense, lazy } from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline'
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div></div>}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+      </MuiThemeProvider>
+    </Suspense>
   );
 }
 
