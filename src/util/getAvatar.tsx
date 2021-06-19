8adcd79f91110486730065c035ed1e39e16f86b2
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-06-19 10:59:09 +0400

    path issues

--- src/util/getAvatar.tsx
+++ src/util/getAvatar.tsx
@@ -2,88 +2,89 @@ export const getAvatar = (id: number) => {
   if (!id) {
     return "";
   }
-  let avatar = "";
+  const assetUrl = process.env.REACT_APP_STATIC_ASSETS_URL as string;
+  let avatar = assetUrl;
   switch (id) {
     case 1:
-      avatar = "avatars/1.svg";
+      avatar += "avatars/1.svg";
       break;
     case 2:
-      avatar = "avatars/2.svg";
+      avatar += "avatars/2.svg";
       break;
     case 3:
-      avatar = "avatars/3.svg";
+      avatar += "avatars/3.svg";
       break;
     case 4:
-      avatar = "avatars/4.svg";
+      avatar += "avatars/4.svg";
       break;
     case 5:
-      avatar = "avatars/5.svg";
+      avatar += "avatars/5.svg";
       break;
     case 6:
-      avatar = "avatars/6.svg";
+      avatar += "avatars/6.svg";
       break;
     case 7:
-      avatar = "avatars/7.svg";
+      avatar += "avatars/7.svg";
       break;
     case 8:
-      avatar = "avatars/8.svg";
+      avatar += "avatars/8.svg";
       break;
     case 9:
-      avatar = "avatars/9.svg";
+      avatar += "avatars/9.svg";
       break;
     case 10:
-      avatar = "avatars/10.svg";
+      avatar += "avatars/10.svg";
       break;
     case 11:
-      avatar = "avatars/11.svg";
+      avatar += "avatars/11.svg";
       break;
     case 12:
-      avatar = "avatars/12.svg";
+      avatar += "avatars/12.svg";
       break;
     case 13:
-      avatar = "avatars/13.svg";
+      avatar += "avatars/13.svg";
       break;
     case 14:
-      avatar = "avatars/14.svg";
+      avatar += "avatars/14.svg";
       break;
     case 15:
-      avatar = "avatars/15.svg";
+      avatar += "avatars/15.svg";
       break;
     case 16:
-      avatar = "avatars/16.svg";
+      avatar += "avatars/16.svg";
       break;
     case 17:
-      avatar = "avatars/17.svg";
+      avatar += "avatars/17.svg";
       break;
     case 18:
-      avatar = "avatars/18.svg";
+      avatar += "avatars/18.svg";
       break;
     case 19:
-      avatar = "avatars/19.svg";
+      avatar += "avatars/19.svg";
       break;
     case 20:
-      avatar = "avatars/20.svg";
+      avatar += "avatars/20.svg";
       break;
     case 21:
-      avatar = "avatars/21.svg";
+      avatar += "avatars/21.svg";
       break;
     case 22:
-      avatar = "avatars/22.svg";
+      avatar += "avatars/22.svg";
       break;
     case 23:
-      avatar = "avatars/23.svg";
+      avatar += "avatars/23.svg";
       break;
     case 24:
-      avatar = "avatars/24.svg";
+      avatar += "avatars/24.svg";
       break;
     case 25:
-      avatar = "avatars/25.svg";
+      avatar += "avatars/25.svg";
       break;
     case 26:
-      avatar = "avatars/26.svg";
+      avatar += "avatars/26.svg";
       break;
     case 27:
-      avatar = "avatars/27.svg";
+      avatar += "avatars/27.svg";
       break;
     default:
       break;
