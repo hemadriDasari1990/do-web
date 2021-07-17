539c2a05f1c4a48e9498666b7dd077248a41fc12
Hemadri Dasari <hemadri.dasari1990@gmail.com>
2021-07-17 11:42:00 +0400

    fixes

--- src/components/Home/summary.tsx
+++ src/components/Home/summary.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useUserLoading, useUserSummary } from "../../redux/state/user";
+import { useUserSummary } from "../../redux/state/user";
 
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
@@ -14,7 +14,6 @@ const Summary = () => {
   const {} = useStyles();
   const dispatch = useDispatch();
   const { summary } = useUserSummary();
-  const { loading } = useUserLoading();
 
   useEffect(() => {
     dispatch(getAllSummary());
@@ -25,76 +24,74 @@ const Summary = () => {
       <Box textAlign="center" pb={3}>
         <Typography variant="h1">Built for global scale</Typography>
       </Box>
-      {!loading && (
-        <Grid container spacing={2}>
-          <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
-            <Hidden only={["xl", "lg", "md"]}>
-              <Box textAlign="center">
-                <Typography variant="h1" style={{ color: getRandomColor(0) }}>
-                  {summary?.usersCount > 1000
-                    ? summary?.usersCount + "+"
-                    : summary?.usersCount || 0}
-                </Typography>
-                <Typography variant="h4">Users Worldwide</Typography>
-              </Box>
-            </Hidden>
-            <Hidden only={["xs", "sm"]}>
-              <Box textAlign="center">
-                <Typography variant="h1" style={{ color: getRandomColor(0) }}>
-                  {summary?.usersCount > 1000
-                    ? summary?.usersCount + "+"
-                    : summary?.usersCount || 0}
-                </Typography>
-                <Typography variant="h2">Users Worldwide</Typography>
-              </Box>
-            </Hidden>
-          </Grid>
-          <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
-            <Hidden only={["xl", "lg", "md"]}>
-              <Box textAlign="center">
-                <Typography variant="h1" style={{ color: getRandomColor(5) }}>
-                  {summary?.projectsCount > 1000
-                    ? summary?.projectsCount + "+"
-                    : summary?.projectsCount || 0}
-                </Typography>
-                <Typography variant="h4">Total projects</Typography>
-              </Box>
-            </Hidden>
-            <Hidden only={["xs", "sm"]}>
-              <Box textAlign="center">
-                <Typography variant="h1" style={{ color: getRandomColor(5) }}>
-                  {summary?.projectsCount > 1000
-                    ? summary?.projectsCount + "+"
-                    : summary?.projectsCount || 0}
-                </Typography>
-                <Typography variant="h2">Total projects</Typography>
-              </Box>
-            </Hidden>
-          </Grid>
-          <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
-            <Hidden only={["xl", "lg", "md"]}>
-              <Box textAlign="center">
-                <Typography variant="h1" style={{ color: getRandomColor(3) }}>
-                  {summary?.boardsCount > 1000
-                    ? summary?.boardsCount + "+"
-                    : summary?.boardsCount || 0}
-                </Typography>
-                <Typography variant="h4">Total boards</Typography>
-              </Box>
-            </Hidden>
-            <Hidden only={["xs", "sm"]}>
-              <Box textAlign="center">
-                <Typography variant="h1" style={{ color: getRandomColor(3) }}>
-                  {summary?.boardsCount > 1000
-                    ? summary?.boardsCount + "+"
-                    : summary?.boardsCount || 0}
-                </Typography>
-                <Typography variant="h2">Total boards</Typography>
-              </Box>
-            </Hidden>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
+          <Hidden only={["xl", "lg", "md"]}>
+            <Box textAlign="center">
+              <Typography variant="h1" style={{ color: getRandomColor(0) }}>
+                {summary?.usersCount > 1000
+                  ? summary?.usersCount + "+"
+                  : summary?.usersCount || 0}
+              </Typography>
+              <Typography variant="h4">Users Worldwide</Typography>
+            </Box>
+          </Hidden>
+          <Hidden only={["xs", "sm"]}>
+            <Box textAlign="center">
+              <Typography variant="h1" style={{ color: getRandomColor(0) }}>
+                {summary?.usersCount > 1000
+                  ? summary?.usersCount + "+"
+                  : summary?.usersCount || 0}
+              </Typography>
+              <Typography variant="h2">Users Worldwide</Typography>
+            </Box>
+          </Hidden>
         </Grid>
-      )}
+        <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
+          <Hidden only={["xl", "lg", "md"]}>
+            <Box textAlign="center">
+              <Typography variant="h1" style={{ color: getRandomColor(5) }}>
+                {summary?.projectsCount > 1000
+                  ? summary?.projectsCount + "+"
+                  : summary?.projectsCount || 0}
+              </Typography>
+              <Typography variant="h4">Total projects</Typography>
+            </Box>
+          </Hidden>
+          <Hidden only={["xs", "sm"]}>
+            <Box textAlign="center">
+              <Typography variant="h1" style={{ color: getRandomColor(5) }}>
+                {summary?.projectsCount > 1000
+                  ? summary?.projectsCount + "+"
+                  : summary?.projectsCount || 0}
+              </Typography>
+              <Typography variant="h2">Total projects</Typography>
+            </Box>
+          </Hidden>
+        </Grid>
+        <Grid item xl={4} lg={4} md={4} sm={4} xs={6}>
+          <Hidden only={["xl", "lg", "md"]}>
+            <Box textAlign="center">
+              <Typography variant="h1" style={{ color: getRandomColor(3) }}>
+                {summary?.boardsCount > 1000
+                  ? summary?.boardsCount + "+"
+                  : summary?.boardsCount || 0}
+              </Typography>
+              <Typography variant="h4">Total boards</Typography>
+            </Box>
+          </Hidden>
+          <Hidden only={["xs", "sm"]}>
+            <Box textAlign="center">
+              <Typography variant="h1" style={{ color: getRandomColor(3) }}>
+                {summary?.boardsCount > 1000
+                  ? summary?.boardsCount + "+"
+                  : summary?.boardsCount || 0}
+              </Typography>
+              <Typography variant="h2">Total boards</Typography>
+            </Box>
+          </Hidden>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
